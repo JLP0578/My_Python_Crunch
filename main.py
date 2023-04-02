a362d29b7cfa81b6a67d059e0398a96db444b6b3
JLP0578 <54902345+JLP0578@users.noreply.github.com>
2023-04-02 13:33:43 +0200

    v2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# import json
+import json
 
-# def write_json(word):
-#     try:
-#         with open('output.txt', 'a', encoding='utf-8') as f:
-#             json.dump(word, f, ensure_ascii=False)
-#     except json.JSONDecodeError as e:
-#             print("Erreur lors de l'écriture :", e.msg)
+def write_json(word):
+    try:
+        with open('output.txt', 'a', encoding='utf-8') as f:
+            json.dump(word, f, ensure_ascii=False)
+    except json.JSONDecodeError as e:
+            print("Erreur lors de l'écriture :", e.msg)
 
 def generate_words(characters, length):
     if length == 0:
@@ -25,7 +25,7 @@ def main():
     length_word = int(input("Entrez la longueur du mot : "))
     if length_word != 0:
         for password in generate_words(list_caracteres, length_word):
-            # write_json(password)
+            write_json(password)
             print(password)
 
 
